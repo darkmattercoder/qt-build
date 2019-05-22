8226a2c460964e0ba470be95b2b5af93a3247353
Jochen Bauer <devel@jochenbauer.net>
2019-05-23 01:58:10 +0200

    trying with system ssl 1.0

--- buildconfig/configure-5.6.0.sh
+++ buildconfig/configure-5.6.0.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-set -e
-set -x
-CORE_COUNT=$1
-# adding compatible ssl version
-git clone https://github.com/openssl/openssl.git
-cd openssl
-git checkout OpenSSL_1_0_1-stable
-LATEST_TAG=$(git describe --match "OpenSSL_1_0_1[a-z]*" --abbrev=0)
-git checkout $LATEST_TAG
-./config --prefix=/opt/openssl_build_stable -shared > /dev/null 2>&1 || ./config --prefix=/opt/openssl_build_stable -shared
-make -j$CORE_COUNT > /dev/null 2>&1 || make -j$CORE_COUNT
-make -j$CORE_COUNT test > /dev/null 2>&1 || make -j$CORE_COUNT test
-make install > /dev/null 2>&1 || make install
-cd ..
-OPENSSL_LIBS='-L/opt/openssl_build_stable/lib -lssl -lcrypto' ../configure -prefix $QT_PREFIX -opensource -confirm-license -nomake examples -nomake tests -qt-xcb -openssl-linked -I /opt/openssl_build_stable/include/openssl -L /opt/openssl_build_stable/lib
+# set -e
+# set -x
+# CORE_COUNT=$1
+# # adding compatible ssl version
+# git clone https://github.com/openssl/openssl.git
+# cd openssl
+# git checkout OpenSSL_1_0_1-stable
+# LATEST_TAG=$(git describe --match "OpenSSL_1_0_1[a-z]*" --abbrev=0)
+# git checkout $LATEST_TAG
+# ./config --prefix=/opt/openssl_build_stable -shared > /dev/null 2>&1 || ./config --prefix=/opt/openssl_build_stable -shared
+# make -j$CORE_COUNT > /dev/null 2>&1 || make -j$CORE_COUNT
+# make -j$CORE_COUNT test > /dev/null 2>&1 || make -j$CORE_COUNT test
+# make install > /dev/null 2>&1 || make install
+# cd ..
+# OPENSSL_LIBS='-L/opt/openssl_build_stable/lib -lssl -lcrypto' ../configure -prefix $QT_PREFIX -opensource -confirm-license -nomake examples -nomake tests -qt-xcb -openssl-linked -I /opt/openssl_build_stable/include/openssl -L /opt/openssl_build_stable/lib
+../configure -prefix $QT_PREFIX -opensource -confirm-license -nomake examples -nomake tests -qt-xcb 
