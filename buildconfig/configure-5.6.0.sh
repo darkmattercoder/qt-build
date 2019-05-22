c105bcaf4a85f16e054fcfa029a8c8b673a8daa3
Jochen Bauer <devel@jochenbauer.net>
2019-05-22 09:46:05 +0200

    make output suppressing

--- buildconfig/configure-5.6.0.sh
+++ buildconfig/configure-5.6.0.sh
@@ -7,8 +7,8 @@ git checkout OpenSSL_1_0_2-stable
 LATEST_TAG=$(git describe --match "OpenSSL_1_0_2[a-z]*" --abbrev=0)
 git checkout $LATEST_TAG
 ./config --prefix=/opt/openssl_build_stable -shared
-make
+make > /dev/null 2>&1 || make
 make test
-make install
+make install > /dev/null || make install
 
 OPENSSL_LIBS='-L/opt/openssl_build_stable/lib -lssl -lcrypto' ../configure -prefix $QT_PREFIX -opensource -confirm-license -nomake examples -nomake tests -qt-xcb -openssl-linked -I /opt/openssl_build_stable/include/openssl -L /opt/openssl_build_stable/lib
