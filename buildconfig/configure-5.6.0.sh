1f301c42ed08cf3ab7de740c04977d0116f6a2e6
Jochen Bauer <jochen.bauer@hydac.com>
2019-05-22 12:04:04 +0200

    config output suppressed

--- buildconfig/configure-5.6.0.sh
+++ buildconfig/configure-5.6.0.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 set -e
+set -x
 # adding compatible ssl version
 git clone https://github.com/openssl/openssl.git
 cd openssl
 git checkout OpenSSL_1_0_2-stable
 LATEST_TAG=$(git describe --match "OpenSSL_1_0_2[a-z]*" --abbrev=0)
 git checkout $LATEST_TAG
-./config --prefix=/opt/openssl_build_stable -shared
+./config --prefix=/opt/openssl_build_stable -shared > /dev/null 2>&1 || ./config --prefix=/opt/openssl_build_stable -shared
 make > /dev/null 2>&1 || make
 make test
-make install > /dev/null || make install
+make install > /dev/null 2>&1 || make install
 
 OPENSSL_LIBS='-L/opt/openssl_build_stable/lib -lssl -lcrypto' ../configure -prefix $QT_PREFIX -opensource -confirm-license -nomake examples -nomake tests -qt-xcb -openssl-linked -I /opt/openssl_build_stable/include/openssl -L /opt/openssl_build_stable/lib
