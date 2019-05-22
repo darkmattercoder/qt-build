e6cc59c1962270ed0acb20fd7660bbf69873194d
Jochen Bauer <devel@jochenbauer.net>
2019-05-22 08:58:17 +0200

    fixed wrong env var assignment

--- buildconfig/configure-5.6.0.sh
+++ buildconfig/configure-5.6.0.sh
@@ -3,7 +3,7 @@ set -e
 # adding compatible ssl version
 git clone https://github.com/openssl/openssl.git
 cd openssl
-LATEST_TAG=git describe --match "OpenSSL_1_0_2[a-z]*" --abbrev=0
+LATEST_TAG=$(git describe --match "OpenSSL_1_0_2[a-z]*" --abbrev=0)
 git checkout $LATEST_TAG
 ./config --prefix=/opt/openssl_build_stable -shared
 make
