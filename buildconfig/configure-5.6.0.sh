8a1cc9cb2b1e95bfa902b408f5399c32c0cf0f06
Jochen Bauer <devel@jochenbauer.net>
2019-05-22 08:34:45 +0200

    trying to build the ssl version for qt to compile

--- buildconfig/configure-5.6.0.sh
+++ buildconfig/configure-5.6.0.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-../configure -prefix $QT_PREFIX -opensource -confirm-license -nomake examples -nomake tests -qt-xcb
+set -e
+# adding compatible ssl version
+git clone https://github.com/openssl/openssl.git
+cd openssl
+LATEST_TAG=git describe --match "OpenSSL_1_0_2[a-z]*" --abbrev=0
+git checkout $LATEST_TAG
+./config --prefix=/opt/openssl_build_stable -shared
+make
+make test
+make install
+
+OPENSSL_LIBS='-L/opt/openssl_build_stable/lib -lssl -lcrypto' ../configure -prefix $QT_PREFIX -opensource -confirm-license -nomake examples -nomake tests -qt-xcb -openssl-linked -I /opt/openssl_build_stable/include/openssl -L /opt/openssl_build_stable/lib
