bf965b873529e4be316b1bbfdba2f7d0e2373c91
Hamish Moffatt <hamish@moffatt.id.au>
2019-09-21 18:18:32 +1000

    Enable MySQL, Postgresql and Interbase drivers

--- buildconfig/configure-5.13.1.sh
+++ buildconfig/configure-5.13.1.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+# Patch Qt to find Firebird library
+sed -i -e 's/"-lgds"/"-lfbclient"/' ../qtbase/src/plugins/sqldrivers/configure.json
+
 ../configure -prefix $QT_PREFIX -opensource -confirm-license -nomake examples -nomake tests
