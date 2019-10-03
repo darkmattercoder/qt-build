cbfa33bccac29c1d19134f411ef5fcbb9ccbff26
Hamish Moffatt <hamish@moffatt.id.au>
2019-10-03 12:16:56 +1000

    add Firebird patch to all versions back to 5.9.0

--- buildconfig/configure-5.13.1.sh
+++ buildconfig/configure-5.13.1.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-# Patch Qt to find Firebird library
 sed -i -e 's/"-lgds"/"-lfbclient"/' ../qtbase/src/plugins/sqldrivers/configure.json
-
 ../configure -prefix $QT_PREFIX -opensource -confirm-license -nomake examples -nomake tests
