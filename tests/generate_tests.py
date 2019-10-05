7830fbaa351db6e521b23e198da68a38e1fad515
devel@jochenbauer.net <devel@jochenbauer.net>
2019-10-05 10:18:30 +0200

    make tests run automagically instead of only building them

--- tests/generate_tests.py
+++ tests/generate_tests.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 ''' generates a tests.pro file by crawling the directory and generating a subdirs
-	entry for each subdirectory '''
+	entry for each subdirectory. Additionally it generates a run script for the tests to get executed,
+    assuming the binary produced is named like the directory'''
 
 import os
 
 with open(os.path.dirname(os.path.realpath(__file__)) + "/tests.pro", "w") as projectfile:
-    projectfile.write("TEMPLATE=subdirs\nSUBDIRS= \\\n")
-    PATH = os.path.dirname(os.path.realpath(__file__))
-    for item in os.listdir(PATH):
-        if os.path.isdir(os.path.join(PATH, item)):
-            projectfile.write(item + " \\\n")
+    with open(os.path.dirname(os.path.realpath(__file__)) + "/run-tests.sh", "w") as runscript:
+        projectfile.write("TEMPLATE=subdirs\nSUBDIRS= \\\n")
+        runscriptContent = "\
+        #!/bin/sh\n\
+        set -e\n\
+        set -x\n\
+        if [ ! -z $1 ]; then\n\
+        TEST_ROOT=$1\n\
+        else\n\
+        TEST_ROOT=.\n\
+        fi\n"
+        runscript.write(runscriptContent)
+        PATH = os.path.dirname(os.path.realpath(__file__))
+        for item in os.listdir(PATH):
+            if os.path.isdir(os.path.join(PATH, item)):
+                projectfile.write(item + " \\\n")
+                runscript.write("timeout 10s $TEST_ROOT/" +
+                                item + "/" + item + "\n")
+os.chmod(os.path.dirname(os.path.realpath(__file__)) + "/run-tests.sh", 0o744)
