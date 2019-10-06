1688e2553a23096dc7397efb48f27482767ee3b2
devel@jochenbauer.net <devel@jochenbauer.net>
2019-10-06 18:43:32 +0200

    improving external lib dependencies

--- buildconfig/configure-5.9.6.sh
+++ buildconfig/configure-5.9.6.sh
@@ -14,5 +14,6 @@ make test > /dev/null 2>&1 || make test
 make install > /dev/null 2>&1 || make install
 cd ..
 OPENSSL_LIBS='-L/opt/openssl_build_stable/lib -lssl -lcrypto' ../configure -prefix $QT_PREFIX -opensource -confirm-license -nomake examples -nomake tests -qt-xcb -openssl-linked -I /opt/openssl_build_stable/include -L /opt/openssl_build_stable/lib
-mkdir /opt/extra-dependencies/opt
-cp -r /opt/openssl_build_stable /opt/extra-dependencies/opt
+mkdir -p /opt/extra-dependencies/lib
+cp -r /opt/openssl_build_stable /opt/extra-dependencies
+ln -s /opt/extra-dependencies/openssl_build_stable/lib/* /opt/extra-dependencies/lib
